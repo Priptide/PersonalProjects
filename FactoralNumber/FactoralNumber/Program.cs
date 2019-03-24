8c37908e0e0e3225cee6b1944e0f568b17a8351c
Priptide <priptidehelp@gmail.com>
2019-03-24 21:45:54 +0000

    Factorial Number Finder
    Finds a factorial for any int up to 12, this is due to the lack of need for larger numbers although longs will allow this too work

---
+++ FactoralNumber/FactoralNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FactoralNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int FinalAnswer = 1;
+            int inputAnswer = new int();
+            Console.WriteLine("Enter a number to find it's factorial! [1 - 12]");
+            string inputString = Console.ReadLine();
+            try
+            {
+                inputAnswer = Convert.ToInt32(inputString);
+                if(inputAnswer > 12)
+                {
+                    Main();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Main();
+            }
+
+            for(int i = 1; i <= inputAnswer; i++)
+            {
+                FinalAnswer = FinalAnswer * i;
+            }
+
+            Console.WriteLine("Answer: " + FinalAnswer.ToString());
+            Console.WriteLine("Again y or n?");
+
+            var answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Main();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
