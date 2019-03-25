3fd8c0c1b13a2816ef13e762ded78781b66bebac
Priptide <priptidehelp@gmail.com>
2019-03-25 21:00:10 +0000

    Lotto Sim
    Picks 6 random numbers between 1 - 49 to allow a lottery like draw!

---
+++ LottoSim/LottoSim/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LottoSim
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start Lotto? [type y to generate numbers]");
+            string inputStr = Console.ReadLine();
+            
+            if(inputStr == "y")
+            {
+                Random rnd = new Random();
+
+                for(int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(rnd.Next(1, 50));
+                }
+
+                Console.WriteLine("Complete! Again? [y to retry]");
+            }
+            else
+            {
+                return;
+            }
+
+            string inputStrN = Console.ReadLine();
+
+            if (inputStr == "y")
+            {
+                Main();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
